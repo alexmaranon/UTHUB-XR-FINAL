ee649a4cfec123783b11fb1a86a38f053184dd98
alexmaranon <alexmaranon077@gmail.com>
2024-11-06 11:28:34 +0100

    setup

---
+++ Source/UTHUB_XR_1/UTHUB_XR_1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UTHUB_XR_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UTHUB_XR_1, "UTHUB_XR_1" );
